46bb6c8aab5b4edb7fb23f0a5e9074f04762465c
Devangi4121 <devangi4121@gmail.com>
2025-06-09 17:56:30 +0530

    remove commented code

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
+              '$_counter Updated',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
