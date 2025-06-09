d3450c2227aa54cf900dc52b2d0a91d8760e03e4
Devangi4121 <devangi4121@gmail.com>
2025-06-09 16:55:44 +0530

    fix: changes done

--- release.config.js
+++ release.config.js
@@ -3,15 +3,25 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/github',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'node scripts/update_pubspec_version.js ${nextRelease.version}'
+      },
+    ],
     [
       '@semantic-release/git',
       {
         assets: ['pubspec.yaml', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-        prepareCmd: `node scripts/update_pubspec_version.js \${nextRelease.version}`
-      }
-    ]
-  ]
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
 };
